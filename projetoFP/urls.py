cfdbcdffc9b7bcef9b30a7c67f4462f6af464ad6
TiagoGR <tiago_ski@hotmail.com>
2014-04-17 20:34:27 -0300

    Enviando arquivos

--- projetoFP/urls.py
+++ projetoFP/urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'projetoFP.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'pessoas.views.index'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^pessoas/', include('pessoas.urlsPessoas')),
+    url(r'^$', 'caixas.views.index'),
+    url(r'^caixas/', include('caixas.urlsCaixas')),
 )
